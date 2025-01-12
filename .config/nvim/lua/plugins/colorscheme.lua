1e060ebdce22125c6898b8f55b058671951c410e
RYoh <yohan.rameau@gmail.com>
2025-01-12 11:10:26 +0100

    Add nvim config

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+
+  { "rebelot/kanagawa.nvim" },
+
+  -- Configure LazyVim to chosen colorscheme
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-dragon",
+    },
+  },
+}
